d726d6af6e6d26fb5281b8139695d5828e20f203
alphak3y <alphak3y@protonmail.com>
2022-11-05 17:36:12 -0600

    bn utils

--- generate_frames.ts
+++ generate_frames.ts
@@ -1,4 +1,5 @@
-import { BigNumberish, BN, Wallet } from "fuels";
+import { BigNumberish, Wallet } from "fuels";
+import BN from "./utils/BN";
 import { BilliardAbi__factory } from "./types";
 
 const initContractId = "0xb021b238d430b7584c8b86b82372984d2b08221eac1d08e37b22227dd2132218";
@@ -7,7 +8,7 @@ export default async function callInit() {
     const wallet = Wallet.generate();
     const contract = BilliardAbi__factory.connect(initContractId, wallet);
     const { transactionId, value } = await contract.functions
-    .init<[BigNumberish], BN>("bar")
+    .init(BN.parseUnits("100", 0).toString(), BN.parseUnits(100,0).toString())
     .call();
     console.log(value);
 }
